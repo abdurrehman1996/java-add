c492e4e92df320a710c77dfbee8b2e98381348c5
Abdur Rehman <abdurrehman01475@gmail.com>
2023-02-02 03:27:12 +0500

    All test cases covered.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(solution(5, 2));
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    static int solution(int param1, int param2) {
+        return param1 + param2;
+    }
+}
